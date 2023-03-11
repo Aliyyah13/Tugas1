693265563f1d17bfc7ffa9567796df46f1d83663
Aliyyah13 <127600174+Aliyyah13@users.noreply.github.com>
2023-03-11 20:32:20 +0800

    Create Asgdll.java

---
+++ Tugas1_SourceCode_Praktek/Asgdll.java
@@ -0,0 +1,16 @@
+/*Nama		: A.Nur Aliyyah
+  Nim		: 13020210016
+  Hari/Tgl	: Kamis, 09 Maret 2023
+  Waktu		: 19.38*/
+  
+public class Asgdll {
+	
+	public static void main(String[] args) {
+	
+		float f= 20.0f;
+		double fll;
+	
+		fll=10.0f;
+		System.out.println ("f : "+f+ "\nf11: "+fll);
+		}
+	}
